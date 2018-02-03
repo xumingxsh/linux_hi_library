07d9daa59858c173b162acffb6b93eb368f76902
root <root@localhost.localdomain>
2018-02-03 11:14:25 +0800

    refactoring hiLog

--- linux_hi_library/hicommon/impl/common/hiLog.cpp
+++ linux_hi_library/hicommon/impl/common/hiLog.cpp
@@ -3,134 +3,157 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdio.h>
-#include <sys/syscall.h>
-#include <sys/epoll.h>
 
-#include <iostream>
+static Hi_Log_CallBack writeLogCallback = NULL;
+static Hi_Log_Filter_Callback filterCallBack = NULL;
+static Hi_Extended_LogInfo_Callback extendedInfoCallback = NULL;
 
-//#include "mutex/hiCritical.h"
+const static int MAX_BUF_SIZE = 2048;
 
-using namespace std;
+static unsigned short logInfoLevel          = 0;
+static unsigned short logTraceLevel         = 0;
+static unsigned short logDebugLevel         = 0;
+static unsigned short logErrorLevel         = 0;
+static unsigned short logCriticalLevel      = 0;
+static unsigned short logAlwaysOutputLevel  = 0;
 
-#if !defined(HILOG_ON_VARLIST)
-#define HILOG_ON_VARLIST(x) \
-	char chArr[2048];\
-	memset(chArr, 0 , sizeof(chArr));\
-	va_list arguments;\
-	va_start(arguments, x);\
-	vsnprintf(chArr, 2047, x, arguments);	\
-	va_end(arguments) ; 
-#endif
+static void writeLogWithCallback(unsigned short logType, const char* file,
+                                 const char* function, int line, const char* fmt, va_list args);
 
-namespace Hi
+static bool isSelected(unsigned short level);
+
+void set_hi_log_callback(Hi_Log_CallBack callback)
 {
-static HiLogCallBack s_callback = NULL;
-static Hi_Log_Type s_log_type = HLT_Info;
-//static HiCritical s_lock;	
-static pid_t gettid()
-{
-	return static_cast<pid_t>(::syscall(SYS_gettid));
-}
-FunctionLog::FunctionLog(const char* name)
-{
-	if ((int)s_log_type > (int)HLT_FunLog)
-	{
-		return;
-	}
-	name_ = name;
-	gettimeofday(&begin_, NULL);
-	hi_log(HLT_FunLog, 
-		"[%s %s ][function:%s][thread:%d][enter]",
-		__DATE__, __TIME__, name, gettid());
-}
-FunctionLog::~FunctionLog()
-{
-	if ((int)s_log_type > (int)HLT_FunLog)
-	{
-		return;
-	}
-	timeval now;
-	gettimeofday(&now, NULL);
-	unsigned long long distance = 
-		(now.tv_sec * 1000 * 1000 + now.tv_usec) - 
-		(begin_.tv_sec * 1000 * 1000 + begin_.tv_usec);		
-	
-	hi_log(HLT_FunLog, 
-		"[%s %s ][function:%s][thread:%d]"
-		"[leave,leave,time long(usecond):%d]",
-		__DATE__, __TIME__, name_.c_str(), 
-		gettid(), distance);
+    writeLogCallback = callback;
 }
 
-void set_log_evel(Hi_Log_Type type)
+void set_hi_log_filter_callback(Hi_Log_Filter_Callback callback)
 {
-	s_log_type = type;
+    filterCallBack = callback;
 }
 
-void set_log_callback(HiLogCallBack callback)
+void set_hi_extended_log_info_callback(Hi_Extended_LogInfo_Callback callback)
 {
-	s_callback = callback;
+    extendedInfoCallback = callback;
 }
 
-void hi_log(Hi_Log_Type type, const char* fmt, ...)
+void set_hi_info_loglevel(unsigned short level)
 {
-	if ((int)s_log_type > (int)type)
-	{
-		return;
-	}
-	
-	HILOG_ON_VARLIST(fmt);
-	
-	if (s_callback != NULL)
-	{
-		//s_lock.lock();
-		s_callback(type, chArr);
-		//s_lock.unlock();
-	}
-	else
-	{
-		//pid_t pid = gettid();
-		//s_lock.lock();
-		/*
-		cout<<"[log type:"<<type<<"]"<<"[thread id:"<<pid<<
-			"]"<<"["<<__DATE__<<" "<<__TIME__<<"]["<<chArr<<"]"<<endl;*/
-		cout<<"["<<__DATE__<<" "<<__TIME__<<"]["<<chArr<<"]"<<endl;
-		//s_lock.unlock();
-	}
-}
-void hi_info(const char* fmt,...)
-{
-	HILOG_ON_VARLIST(fmt);
-	hi_log(HLT_Info, chArr);
-}
-void hi_warn(const char* fmt,...)
-{
-	HILOG_ON_VARLIST(fmt);
-	hi_log(HTL_Warn, chArr);
-}
-void hi_error(const char* fmt,...)
-{
-	HILOG_ON_VARLIST(fmt);
-	hi_log(HTL_Error, chArr);
-}
-void hi_error(const char* fun, const char* fmt,...)
-{
-	HILOG_ON_VARLIST(fmt);
-	hi_log(HTL_Error, "%s %s", fun, chArr);
+    logInfoLevel = level;
 }
 
-HiErrorInfo::HiErrorInfo(const char* fun):function_(fun)
+void set_hi_debug_loglevel(unsigned short level)
 {
+    logDebugLevel = level;
 }
 
-void HiErrorInfo::log(const char* fmt, ...)
+void set_hi_critical_loglevel(unsigned short level)
 {
-	HILOG_ON_VARLIST(fmt);
-	hi_error("%s %s", function_.c_str(), chArr);
+    logCriticalLevel = level;
 }
 
+void set_hi_trace_loglevel(unsigned short level)
+{
+    logTraceLevel = level;
 }
 
+void set_hi_error_loglevel(unsigned short level)
+{
+    logErrorLevel = level;
+}
 
-	
-	
+void set_hi_always_output_loglevel(unsigned short level)
+{
+    logAlwaysOutputLevel = level;
+}
+
+bool is_using_extended_info()
+{
+    return extendedInfoCallback != NULL;
+}
+
+#define WRITE_LOG(level) \
+	if (!isSelected(level)) \
+    { \
+        return;\
+    }\
+    va_list arguments;\
+    va_start(arguments, fmt);\
+    writeLogWithCallback(level, file, function, line, fmt, arguments);\
+    va_end(arguments)
+
+void hilog_writeLog(const char* file, const char* function, int line, unsigned short logLevel, const char* fmt, ...)
+{
+	WRITE_LOG(logLevel);
+}
+
+void hilog_writeInfoLog(const char* file, const char* function, int line, const char* fmt, ...)
+{
+	WRITE_LOG(logInfoLevel);
+}
+
+void hilog_writeTraceLog(const char* file, const char* function, int line, const char* fmt, ...)
+{
+	WRITE_LOG(logTraceLevel);
+}
+
+void hilog_writeDebugLog(const char* file, const char* function, int line, const char* fmt, ...)
+{
+	WRITE_LOG(logDebugLevel);
+}
+
+void hilog_writeCriticalLog(const char* file, const char* function, int line, const char* fmt, ...)
+{
+	WRITE_LOG(logCriticalLevel);
+}
+
+void hilog_writeErrorLog(const char* file, const char* function, int line, const char* fmt, ...)
+{
+	WRITE_LOG(logErrorLevel);
+}
+
+void hilog_writeAlwaysOutputLog(const char* file, const char* function, int line, const char* fmt, ...)
+{
+	WRITE_LOG(logAlwaysOutputLevel);
+}
+
+static void writeLogWithCallback(unsigned short logLevel, const char* file,
+                                 const char* function, int line, const char* fmt, va_list args)
+{
+    char chArr[MAX_BUF_SIZE];
+    memset(chArr, 0, sizeof(chArr));
+
+    vsnprintf(chArr, MAX_BUF_SIZE, fmt, args);
+
+
+    if (NULL == extendedInfoCallback)
+    {
+        writeLogCallback(logLevel, chArr);
+        return;
+    }
+
+    Hi_Extended_LogInfo info;
+    memset(&info, 0, sizeof(Hi_Extended_LogInfo));
+    extendedInfoCallback(logLevel, file, function, line, info);
+
+    info.info[HI_MAX_LOG_FILE_NAME_LINE_LENGTH - 1] = '\0';
+
+    char item[MAX_BUF_SIZE + HI_MAX_LOG_FILE_NAME_LINE_LENGTH] = {0};
+
+    snprintf(item, sizeof(item), "%s%s", info.info, chArr);
+    writeLogCallback(logLevel, item);
+}
+
+static bool isSelected(unsigned short level)
+{
+    if (NULL == writeLogCallback)
+    {
+        return false;
+    }
+
+    if (NULL == filterCallBack)
+    {
+        return true;
+    }
+    return filterCallBack(level);
+}
--- linux_hi_library/hicommon/include/common/hiLog.h
+++ linux_hi_library/hicommon/include/common/hiLog.h
@@ -1,63 +1,156 @@
-﻿#pragma once
-#include <string>
-#include <sys/time.h>
+﻿#ifndef HI_LOG_HEAD_H
+#define HI_LOG_HEAD_H
 
-namespace Hi
-{
-enum Hi_Log_Type
-{
-	HLT_FunLog,	/// < 函数的进入，离开时间
-	HLT_Info,	/// < 普通日志
-	HTL_Warn,	/// < 警告日志
-	HTL_Error	/// < 错误日志
-};
-typedef void (*HiLogCallBack)(Hi_Log_Type type, const char* script);
-class FunctionLog
-{
-public:
-	FunctionLog(const char* name);
-	~FunctionLog();
-private:
-	std::string name_;
-	std::string param_;
-	timeval begin_;
-};
+#define HI_LOG_INFO(format, ...) \
+    hilog_writeInfoLog(__FILE__,__FUNCTION__, __LINE__, format, __VA_ARGS__)
 
-void set_log_callback(HiLogCallBack callback);
-void set_log_evel(Hi_Log_Type type);
-void hi_log(Hi_Log_Type type, const char* fmt,...);
-void hi_info(const char* fmt,...);
-void hi_warn(const char* fmt,...);
-void hi_error(const char* fmt,...);
+#define HI_LOG_TRACE(format, ...) \
+    hilog_writeTraceLog(__FILE__,__FUNCTION__, __LINE__, format, __VA_ARGS__)
 
-class HiErrorInfo
+#define HI_LOG_DEBUG(format, ...) \
+    hilog_writeDebugLog(__FILE__,__FUNCTION__, __LINE__, format, __VA_ARGS__)
+
+#define HI_LOG_CRITICAL(format, ...) \
+    hilog_writeCriticalLog(__FILE__,__FUNCTION__, __LINE__, format, __VA_ARGS__)
+
+#define HI_LOG_ERROR(format, ...) \
+    hilog_writeErrorLog(__FILE__,__FUNCTION__, __LINE__, format, __VA_ARGS__)
+
+#define HI_LOG_ALWAYS_OUTPUT(format, ...) \
+    hilog_writeAlwaysOutputLog(__FILE__,__FUNCTION__, __LINE__, format, __VA_ARGS__)
+
+/*
+ *  * @brief The macro of write log.
+ *   * @note This macro is not recommanded.
+ *    *        It is only used when you log level is not in info/trace/debug/critical/error.
+ *    */
+#define OMCI_LOG_WRITE(logLevel, format, ...) \
+     hilog_writeLog(__FILE__,__FUNCTION__, __LINE__, logLevel, format, __VA_ARGS__)
+	
+extern "C"
 {
-public:
-	HiErrorInfo(const char* fun);
-	void log(const char* fmt, ...);
-private:
-	std::string function_;
-};
+/*
+ *  * @brief The callback of write log
+ *   * @param[in] logLevel log level
+ *    * @param[in] log log content
+ *    */
+typedef void (*Hi_Log_CallBack)(unsigned short log_level, const char* log);
+
+/*
+ *  * @brief Set the callback of write log
+ *   * @param[in] callback
+ *    * @note User need provide the implement.
+ *     *       It will add function name,file, line number  and so on Automatically.
+ *      *       If user not call this function or callback is NULL, then it will output nothing
+ *      */
+void set_hi_log_callback(Hi_Log_CallBack callback);
+
+/*
+ *  * @brief Set user log level value, it will used by HI_LOG_INFO
+ *   * @param[in] level  log level value of info
+ *    * @note If this function not called, the HI_LOG_INFO will use zero as it's loglevel
+ *    */
+void set_hi_info_loglevel(unsigned short level);
+
+/*
+ *  * @brief Set user log level value, it will used by HI_LOG_TRACE
+ *   * @param[in] level  log level value of trace
+ *    * @note If this function not called, the HI_LOG_TRACE will use zero as it's loglevel
+ *    */
+void set_hi_trace_loglevel(unsigned short level);
+
+/*
+ *  * @brief Set user log level value, it will used by HI_LOG_DEBUG
+ *   * @param[in] level  log level value of debug
+ *    * @note If this function not called, the HI_LOG_DEBUG will use zero as it's loglevel
+ *    */
+void set_hi_debug_loglevel(unsigned short level);
+
+/*
+ *  * @brief Set user log level value, it will used by HI_LOG_CRITICAL
+ *   * @param[in] level  log level value of critical
+ *    * @note If this function not called, the HI_LOG_CRITICAL will use zero as it's loglevel
+ *    */
+void set_hi_critical_loglevel(unsigned short level);
+
+/*
+ *  * @brief Set user log level value, it will used by HI_LOG_ERROR
+ *   * @param[in] level log level value of error
+ *    * @note If this function not called, the HI_LOG_ERROR will use zero as it's loglevel
+ *    */
+void set_hi_error_loglevel(unsigned short level);
+
+/*
+ *  * @brief The level which always output.
+ *   * @param[in] level Wether always output depended on your implement.
+ *    * @note In many cases, Developer using error ot cirtical level to write log,
+ *     *       just to output the log to screen or file， Those log not error, also not cirtical.
+ *      *         so we provider this level for this.
+ *       *       Wether always output, depend on the log which user provider.
+ *        *       Default this level is 0, maybe the log not output
+ *        */
+void set_hi_always_output_loglevel(unsigned short level);
+
+
+/*
+ *  * @brief The callback of log filter
+ *   * @param[in] level log level
+ *    * @retval true: the log will output in this level; false: the log will not ouput in this level
+ *    */
+typedef bool (*Hi_Log_Filter_Callback)(unsigned short level);
+
+/*
+ *  * @brief Set the callback of log filter
+ *   * @param[in] callback
+ *    * @note If user not call this function or callback is NULL,
+ *     *       all log will output
+ *     */
+void  set_hi_log_filter_callback(Hi_Log_Filter_Callback callback);
+
+
+#define HI_MAX_LOG_FILE_NAME_LINE_LENGTH 256
+
+/*
+ *  * @brief The log extended information
+ *   * @note We want the extended information include file, function, position and so on.
+ *   */
+typedef struct Hi_Extended_LogInfo
+{
+    char info[HI_MAX_LOG_FILE_NAME_LINE_LENGTH];
+} Hi_Extended_LogInfo;
+
+/*
+ *  * @brief The callback of write HI log extended information.
+ *   * @param[in] log level May be user want add flag of some level flag in extended information
+ *    * @param[in] file
+ *     * @param[in] function
+ *      * @param[in] line
+ *       * @param[in/out] info this parameter is memset to '0' when it inputed
+ *        * @note we will set last bit of Hi_Extended_LogInfo.info to '\0'
+ *        */
+typedef void (*Hi_Extended_LogInfo_Callback)(unsigned short level, const char* file,
+                                        const char* function, int line, Hi_Extended_LogInfo& info);
+
+/*
+ *  * @brief User can use this function to set his extended information of log.
+ *   * @param[in] callback
+ *    * @note If callback is NULL, don't add extended information to log.
+ *     *       Default is NULL
+ *     */
+void set_hi_extended_log_info_callback(Hi_Extended_LogInfo_Callback callback);
+
+bool is_using_extended_info();
 }
-#ifdef USE_FUN_LOG
-#ifndef FUN_LOG
-#define FUN_LOG() Hi::FunctionLog fun_log_##__LINE__(__FUNCTION__)
-#endif
-#else
-#ifndef FUN_LOG
-#define FUN_LOG() 
-#endif
-#endif
 
+extern "C"
+{	
+void hilog_writeLog(const char* file, const char* function, int line, unsigned short logLevel, const char* fmt, ...);
+void hilog_writeInfoLog(const char* file, const char* function, int line, const char* fmt, ...);
+void hilog_writeTraceLog(const char* file, const char* function, int line, const char* fmt, ...);
+void hilog_writeDebugLog(const char* file, const char* function, int line, const char* fmt, ...);
+void hilog_writeCriticalLog(const char* file, const char* function, int line, const char* fmt, ...);
+void hilog_writeErrorLog(const char* file, const char* function, int line, const char* fmt, ...);
+void hilog_writeAlwaysOutputLog(const char* file, const char* function, int line, const char* fmt, ...);
+}
 
-#ifndef HI_ERROR
-#define HI_ERROR(x)  \
-	{\
-		Hi::HiErrorInfo(__FUNCTION__).log x;\
-	}while(0)
-#endif
-
-
-
-	
-	
+#endif // HI_LOG_HEAD_H
